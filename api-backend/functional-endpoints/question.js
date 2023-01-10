4321d3cd24123930644637000a2d7d270810d010
NikolasStamat <el19020@mail.ntua.gr>
2023-01-10 19:50:17 +0200

    Final csv + schema slight updt

--- api-backend/functional-endpoints/question.js
+++ api-backend/functional-endpoints/question.js
@@ -19,7 +19,7 @@ router.get('/:questionnaireID/:questionID', function(req, res) {
 
         // TODO: needs to be tested + ordered by dec -nat
 		else{
-		q = `select Question.question_text, required, Question.type, Option.option_id, Option.option_text, Option.Question_nextquestion_id from Question inner join Option ON Question.question_id = Option.Question_question_id where (Question.Questionnaire_questionnaire_id = ${questionnaireID} AND Question.question_id = ${questionID})`;	
+		q = `select Question.question_text, Question.required, Question.type, Option.option_id, Option.option_text, Option.Question_nextquestion_id from Question inner join Option ON Question.question_id = Option.Question_question_id where (Question.Questionnaire_questionnaire_id = ${questionnaireID} AND Question.question_id = ${questionID})`;	
 		connection.query(q, function(err, result) {
 
         	if(err) {
@@ -31,29 +31,52 @@ router.get('/:questionnaireID/:questionID', function(req, res) {
     			for (const row of result) {
       				const option = { optID: row.option_id, opttxt: row.option_text, nextqID: row.Question_nextquestion_id };
       				options.push(option);
+					question_text = row.question_text;
+					required = row.required;
+					type = row.type;
     			}
 				
 				console.log(options);
 
 				//generate response
-				const response = {
-						"questionnaireID":questionnaireID,
-						"qID":questionID,
-						"qtext":result.question_text,
-						"required":result.required,
-						"type":result.type,
-                        "options":options
-					//result.rows //only some columns?
+
+				  
+				if(req.query.format === "csv") {
+					const csv_input = [];
+					for (const row of result) {
+						const inputty = {
+							"questionnaireID":questionnaireID,
+							"qID":questionID,
+							"qtext":row.question_text,
+							"required":row.required,
+							"type":row.type,
+                     		"optID": row.option_id.toString(),
+							"opttxt": row.option_text,
+							"nextqID": row.Question_nextquestion_id.toString()
+					}
+					csv_input.push(inputty)
 				}
 
-				if(req.query.format === "csv") {
-					const csvHeader = ['questionnaireID,qID,qtext,required,type,options'];
+					const csvHeader = ['questionnaireID,qID,qtext,required,type,optID,opttxt,nextqID'];
                     const csvObj = { csvHeader };
-                    var csvData = parse(response, csvObj);
+                    var csvData = parse(csv_input, csvObj);
                     res.status(200).send(csvData);
                     console.log("Question info OK.");
+
 				}
 				else {
+					const input = {
+						"questionnaireID":questionnaireID.toString(),
+						"qID":questionID.toString(),
+						"qtext":question_text.toString(),
+						//"required":required,
+						"type":type,
+                        "options":options
+				}
+					const json = JSON.stringify(input);
+               		const response = JSON.parse(json, (key, val) => (
+                    	typeof val !== 'object' && val !== null ? String(val) : val
+                  ));
                     // JSON response: default if no query format specified.					
 					res.status(200).json(response);
 					console.log("Question info OK.");
