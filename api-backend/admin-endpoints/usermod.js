8c32a4a60fad47a5cd7107a01a169b8d2d67d8af
nbourdi <el19031@mail.ntua.gr>
2023-01-11 15:58:37 +0200

    Usermod done & users fix

--- api-backend/admin-endpoints/usermod.js
+++ api-backend/admin-endpoints/usermod.js
@@ -1,10 +1,8 @@
 const express = require('../node_modules/express')
 const router = express.Router();
 const pool = require('../connect');
-const { parse } = require('../node_modules/json2csv');
 
 
-// TODO: add support for CHANGING password, not just adding users
 router.post('/:username/:password', function(req, res) {
 	const { username, password } = req.params;
 	pool.getConnection(function(err, connection) {
@@ -12,17 +10,48 @@ router.post('/:username/:password', function(req, res) {
 			res.status(500).json({status:"failed"});
 			console.log("Failed to connect to database.", err);
 		}
-		connection.query(`insert into User(username, password) values ('${username}', '${password}')`, function(err, result) 
-		{
+		else {
+		q = `select username from User where username = '${username}'`;
+
+		connection.query(q, function(err, result) {
+
 			if(err) {
-				res.status(400).json({status:"failed"});
-				console.log("Usermod bad request", err);
+				res.status(500).json({status:"failed", reason: "Error getting user information."});
+				console.log(err);
 			}
 			else {
-				res.status(200).json({status:"OK"});
-                console.log("Usermod success.");
-			}
-   		});
+					if(result.length == 0) {
+						// Empty set response -> add user functionality
+						connection.query(`insert into User(username, password) values ('${username}', '${password}')`, function(err, result) 
+						{
+							if(err) {
+								res.status(400).json({status:"failed"});
+								console.log("Usermod bad request", err);
+							}
+							else {
+								res.status(200).json({status:"OK"});
+								console.log("User successfully added.");
+							}
+						});
+					}
+					
+					else {
+						// User exists in the database -> change password functionality
+						connection.query(`update User set password = '${password}' where username = '${username}'`, function(err, result) 
+						{
+							if(err) {
+								res.status(400).json({status:"failed"});
+								console.log("Update failed.", err);
+							}
+							else {
+								res.status(200).json({status:"OK"});
+								console.log("Password successfully changed.");
+							}
+						});
+					}
+				}	
+			});
+		}
 		connection.release();
 	});
 });
