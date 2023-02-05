e22598c4c92e97eb41772d4f3e1ba39223c7a0dd
ntua-el19065 <115083302+ntua-el19065@users.noreply.github.com>
2023-02-05 13:11:19 +0200

    Reupload Routes

---
+++ api-backend/routes/routes_questionnaire.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router();
+
+router.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, '../..') + "/frontend/frontend1/build/index.html");
+});
+const path = require('path')
+const express = require('express')
+const router = express.Router();
+
+router.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, '../..') + "/frontend/frontend1/build/index.html");
+});
+
+module.exports = router;
