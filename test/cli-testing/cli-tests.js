91f2e02274911e9257c30904de4deed8beacd650
ntua-el19020 <115226054+ntua-el19020@users.noreply.github.com>
2023-02-10 21:08:16 +0200

    Update cli-tests.js

--- test/cli-testing/cli-tests.js
+++ test/cli-testing/cli-tests.js
@@ -5,6 +5,19 @@ const assert = require('chai').assert;
 describe('se2213 questionnaire --questionnaire_id <id> --format <json>', function () {
   //TEST QUESTIONNAIRES WITH ID 1,...,5
   for (let i = 1; i < 6; i++) {
+    it(`should have exit code 0  (id = ${i})`, function (done) {
+      const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--format', 'json']);
+      let output = '';
+
+      cli.stdout.on('data', function (data) {
+        output += data.toString();
+      });
+
+      cli.on('close', function (code) {
+        assert.equal(code, 0);
+        done();
+      });
+    });
     it(`should return status: OK (id = ${i})`, function (done) {
       const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--format', 'json']);
       let output = '';
@@ -59,6 +72,19 @@ describe('se2213 questionnaire --questionnaire_id <id> --format <json>', functio
 
 describe('se2213 questionnaire --questionnaire_id <id = 1> --format <csv>', function () {
   for (let i = 1; i < 6; i++) {
+    it(`should have exit code 0  (id = ${i})`, function (done) {
+      const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--format', 'csv']);
+      let output = '';
+
+      cli.stdout.on('data', function (data) {
+        output += data.toString();
+      });
+
+      cli.on('close', function (code) {
+        assert.equal(code, 0);
+        done();
+      });
+    });
     it(`should return status: OK (id = ${i})`, function (done) {
       const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--format', 'csv']);
       let output = '';
@@ -101,6 +127,19 @@ describe('se2213 questionnaire --questionnaire_id <id = 1> --format <csv>', func
 describe('se2213 question --questionnaire_id <id> --question_id <id> --format <json>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++){
+      it(`should have exit code 0 (questionnaire = ${i}, qid = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--question_id', j, '--format', 'json']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, qid = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--question_id', j, '--format', 'json']);
         let output = '';
@@ -156,6 +195,19 @@ describe('se2213 question --questionnaire_id <id> --question_id <id> --format <j
 describe('se2213 question --questionnaire_id <id = 1> --question_id <id = 2> --format <csv>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++){
+      it(`should have exit code 0 (questionnaire = ${i}, qid = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--question_id', j, '--format', 'csv']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, qid = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'questionnaire', '--questionnaire_id', i, '--question_id', j, '--format', 'csv']);
         let output = '';
@@ -200,6 +252,19 @@ describe('se2213 question --questionnaire_id <id = 1> --question_id <id = 2> --f
 describe('se2213 getquestionanswers --questionnaire_id <id> --question_id <id> --format <json>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++) {
+      it(`should have exit code 0 (questionnaire = ${i}, qid = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'getquestionanswers', '--questionnaire_id', i, '--question_id', j, '--format', 'json']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, qid = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'getquestionanswers', '--questionnaire_id', i, '--question_id', j, '--format', 'json']);
         let output = '';
@@ -250,6 +315,19 @@ describe('se2213 getquestionanswers --questionnaire_id <id> --question_id <id> -
 describe('se2213 getquestionanswers --questionnaire_id <id> --question_id <id> --format <csv>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++) {
+      it(`should have exit code 0 (questionnaire = ${i}, qid = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'getquestionanswers', '--questionnaire_id', i, '--question_id', j, '--format', 'csv']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, qid = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'getquestionanswers', '--questionnaire_id', i, '--question_id', j, '--format', 'csv']);
         let output = '';
@@ -284,10 +362,59 @@ describe('se2213 getquestionanswers --questionnaire_id <id> --question_id <id> -
   }
 });
 
+//TEST doanswer CALL, 5 questions from 5 questionnaires for session_id = 'TeSt'
+describe('se2213 doanswer --questionnaire_id <id> --question_id <id> --session_id <id> --option_id <id>', function () {
+  for (let i = 1; i < 6; i++) {
+    for (let j=1; j < 6; j++) {
+      it(`should have exit code 0 (questionnaire = ${i}, question = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'doanswer', '--questionnaire_id', i, '--question_id', j, '--session_id', 'TeSt', '--option_id', '1']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          assert.notInclude(output, 'failed');
+          done();
+        });
+      });
+      it(`should return status: OK (questionnaire = ${i}, question = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'doanswer', '--questionnaire_id', i, '--question_id', j, '--session_id', 'TeSt', '--option_id', '1']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          assert.include(output, 'OK');
+          done();
+        });
+      });
+    }
+  }
+});
+
 //TEST getsessionanswers CALL, 5 sessions from 5 questionnaires
 describe('se2213 getsessionanswers --questionnaire_id <id> --session_id <id> --format <json>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++) {
+      it(`should have exit code 0 (questionnaire = ${i}, session = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'getsessionanswers', '--questionnaire_id', i, '--session_id', j, '--format', 'json']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, session = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'getsessionanswers', '--questionnaire_id', i, '--session_id', j, '--format', 'json']);
         let output = '';
@@ -338,6 +465,19 @@ describe('se2213 getsessionanswers --questionnaire_id <id> --session_id <id> --f
 describe('se2213 getsessionanswers --questionnaire_id <id> --session_id <id> --format <csv>', function () {
   for (let i = 1; i < 6; i++) {
     for (let j=1; j < 6; j++) {
+      it(`should have exit code 0 (questionnaire = ${i}, session = ${j})`, function (done) {
+        const cli = spawn('node', ['./se2213.js', 'getsessionanswers', '--questionnaire_id', i, '--session_id', j, '--format', 'csv']);
+        let output = '';
+  
+        cli.stdout.on('data', function (data) {
+          output += data.toString();
+        });
+  
+        cli.on('close', function (code) {
+          assert.equal(code, 0);
+          done();
+        });
+      });
       it(`should return status: OK (questionnaire = ${i}, session = ${j})`, function (done) {
         const cli = spawn('node', ['./se2213.js', 'getsessionanswers', '--questionnaire_id', i, '--session_id', j, '--format', 'csv']);
         let output = '';
@@ -372,7 +512,7 @@ describe('se2213 getsessionanswers --questionnaire_id <id> --session_id <id> --f
   }
 });
 
-//test for healthcheck
+//TEST for healthcheck
 describe('se2213 healthcheck', function () {
   it('should show status and dbconnection', function (done) {
     const cli = spawn('node', ['./se2213.js', 'healthcheck']);
