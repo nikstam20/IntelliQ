96b5520ff43fb24cc1b3940cc13fde9f2f092cf7
ntua-el19065 <115083302+ntua-el19065@users.noreply.github.com>
2023-02-10 22:37:57 +0200

    Update healthcheck.js

--- api-backend/admin-endpoints/healthcheck.js
+++ api-backend/admin-endpoints/healthcheck.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const pool = require('../connect');
 
 router.get('/', function(req, res) {
-	pool.getConnection(function(err, connection) {
+	pool.getConnection(function(err, connection) {			//get a connection to the database from the pool
 		if(err) {
-		        res.status(500).json({status:"failed", "dbconnection":"mysql://root:password@localhost:9103/intelliq"});
+		        res.status(500).json({status:"failed", "dbconnection":"mysql://root:password@localhost:9103/intelliq"});	//if there is an error return status 500 (Internal Server Error)
                         console.log("Connection could not be established.", err);
   		}
 		else {
 			res.status(200).json({status:"OK", "dbconnection":"mysql://root:password@localhost:9103/intelliq"});
                        	console.log("Successful connection to MySQL database.");
 		}
-		connection.release();
+		connection.release();		//lastly release the connection so someone else can take it
 	});
 });
 
