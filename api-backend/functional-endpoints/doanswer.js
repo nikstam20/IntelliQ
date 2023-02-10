ed38954bb6a017463b3db2aa661a249375bc9682
ntua-el19020 <115226054+ntua-el19020@users.noreply.github.com>
2023-02-10 20:31:38 +0200

    Update doanswer.js
    uploaded correct version

--- api-backend/functional-endpoints/doanswer.js
+++ api-backend/functional-endpoints/doanswer.js
@@ -1,31 +1,38 @@
 const express = require('../node_modules/express');
 const router = express.Router();
 const pool = require('../connect');
-var id= 1;
+var id = 1;
 
 
-router.post('/:questionnaireID/:questionID/:session/:optionID', function(req, res) {
-const { questionnaireID, questionID, session, optionID } = req.params;
-pool.getConnection(function(err, connection) {
-		if(err) {
-			res.status(500).json({status:"failed"});
+router.post('/:questionnaireID/:questionID/:session/:optionID', function (req, res) {
+	const { questionnaireID, questionID, session, optionID } = req.params;
+	pool.getConnection(function (err, connection) {
+		if (err) {
+			res.status(500).json({ status: "failed" });
 			console.log("Failed to connect to database.", err);
 		}
-        //paizei na exei themataki to post gt den tou dinoume timi sto answer_id
-		connection.query(`insert into Answer(Option_option_id,Option_questionnaire_id,Option_question_id, Session_session_id) values ('${optionID}', '${questionnaireID}','${questionID}','${session}')`, function(err, result) 
-		{
-			if(err) {
-				res.status(400).json({status:"failed"});
+
+		connection.query(`insert ignore into Session(session_id, Questionnaire_questionnaire_id) values ('${session}', '${questionnaireID}')`, function (err, result) {
+			if (err) {
+				res.status(400).json({ status: "failed" });
 				console.log("Usermod bad request", err);
 			}
 			else {
-				res.status(200).json({status:"OK"});
-                console.log("answer success.");
+				console.log("created session if it didn't exist");
+				connection.query(`insert into Answer(Option_option_id,Option_questionnaire_id,Option_question_id, Session_session_id) values ('${optionID}', '${questionnaireID}','${questionID}','${session}')`, function (err, result) {
+					if (err) {
+						res.status(400).json({ status: "failed" });
+						console.log("Usermod bad request", err);
+					}
+					else {
+						res.status(200).json({ status: "OK" });
+						console.log("answer success.");
+					}
+				});
 			}
-   		});
+		});
 		connection.release();
 	});
 });
-
 
 module.exports = router;
