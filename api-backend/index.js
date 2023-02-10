a5289873e1bb96622b16cd0df8dbfd2cbb501979
ntua-el19065 <115083302+ntua-el19065@users.noreply.github.com>
2023-02-10 22:36:35 +0200

    Update index.js

--- api-backend/index.js
+++ api-backend/index.js
@@ -16,11 +16,12 @@ const server = https.createServer(/*{ key, cert },*/ app);
 const webserver = https.createServer(/*{ key, cert },*/ webapp);
 
 // API WEB SERVER
-app.listen(PORT, () => {
-	console.log(`app listening at: http://localhost:${PORT}${baseurl}`);
+
+app.listen(PORT, () => {								//rest api listening to port 9103 and
+	console.log(`app listening at: http://localhost:${PORT}${baseurl}`);		//creating url to access endpoints from
 });
 
-app.get(baseurl, function (req,res) {
+app.get(baseurl, function (req,res) {											
 	res.send("InteliQ IS UP!");
 });
 
@@ -30,20 +31,20 @@ app.use(cors());
 app.use(fileupload());
 app.use(express.urlencoded({ extended: true }));
 
-// WEB SERVER FOR FRONT-END 
-webapp.listen(80, () => {
- 	console.log('Web-server is up and runing at: http://www.inteliQ.com');
+// WEB SERVER (for frontend)
+webapp.listen(80, () => {								//create a web server listening to port 80
+ 	console.log('Web-server is up and runing at: http://www.inteliQ.com');		//and link it to www.inteliQ.com
  });
 
 webapp.get("/", function (req,res) {
 	res.send("Webserver IS UP!");
 });
 
-const adminhealth = require('./admin-endpoints/healthcheck'),
+const adminhealth = require('./admin-endpoints/healthcheck'),					//set from where to get each endpoint
 	questionnaireupd = require('./admin-endpoints/questionnaire_upd'),
 	resetall = require('./admin-endpoints/resetall'),
 	resetq = require('./admin-endpoints/resetq'),
-    // login = require('./admin-endpoints/usermod'),
+    // login = require('./admin-endpoints/usermod'),	these two are not yet implemented
     // logout = require('./admin-endpoints/users'),
 	questionnaire = require('./functional-endpoints/questionnaire'),
 	question = require('./functional-endpoints/question'),
@@ -54,8 +55,8 @@ const adminhealth = require('./admin-endpoints/healthcheck'),
 const { homedir } = require('os');
 
 // RESTFUL API ROUTES
-app.use(baseurl+'/admin/healthcheck', adminhealth);
-app.use(baseurl+'/admin/questionnaire_upd', questionnaireupd);
+app.use(baseurl+'/admin/healthcheck', adminhealth);					//and set the paths from the base url we created before
+app.use(baseurl+'/admin/questionnaire_upd', questionnaireupd);				// to access the endpoints through the url
 app.use(baseurl+'/admin/resetall', resetall);
 app.use(baseurl+'/admin/resetq', resetq);
 // app.use(baseurl+'/admin/usermod', login)
@@ -71,6 +72,5 @@ app.use(baseurl+'/getquestionanswersenhanced', getquestionanswers_enhanced);
 // ROUTES FOR FRONTEND
 
 webapp.use(express.static(path.join(__dirname, '..') + "/frontend/frontend1/build"));
-webapp.use("/questionnaire", require('./routes/routes_questionnaire.js'));
 
 module.exports = router;
