30a07695375db0aa7ab6910aa39ee80257c28faa
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-02-11 15:09:52 +0200

    update resetq.js

--- api-backend/admin-endpoints/resetq.js
+++ api-backend/admin-endpoints/resetq.js
@@ -4,14 +4,14 @@ const pool = require('../connect');
 
 router.post('/:questionnaireID', function(req, res) {
 	const { questionnaireID } = req.params;
-	pool.getConnection(function(err, connection) {
+	pool.getConnection(function(err, connection) {                 //get a connection to the database from the pool
 		if(err) {
-			console.log("Connection failed", err);
-			res.status(500).json({status:"failed"});
+			console.log("Connection failed", err);         
+			res.status(500).json({status:"failed"});       //if there is an error return status 500 (Internal Server Error)
 				
 		}
-		q = `delete from Answer where Answer.Option_questionnaire_id = ${questionnaireID};`; 
-		connection.query(q, function(err, result) {
+		q = `delete from Answer where Answer.Option_questionnaire_id = ${questionnaireID};`;    // query for deleting a specific answer
+		connection.query(q, function(err, result) {                                             // Executing the query, if any error happens return error 400 status failed (Bad Request)
         	if(err) {
 				console.log(err);
 				return res.status(400).json({status:"failed", reason: "Couldn't delete answers"});
@@ -19,10 +19,10 @@ router.post('/:questionnaireID', function(req, res) {
 			}
 			else {
 				console.log("Answers for questionnaire deleted.");
-				res.status(200).json({status:"OK"});
+				res.status(200).json({status:"OK"});                                     // and if everything worked correctly return status 200 OK
 			}
 		});
-		connection.release();
+		connection.release();                                                                    // release the connection so someone else can use it
 	});
 });
 
