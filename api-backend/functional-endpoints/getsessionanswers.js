d1697d7774e717d1bd225220509d995dc4832894
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-02-11 15:28:49 +0200

    Update getsessionanswers.js

--- api-backend/functional-endpoints/getsessionanswers.js
+++ api-backend/functional-endpoints/getsessionanswers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool  = require('../connect');
 const { parse } = require('../node_modules/json2csv');
 
-router.get('/:questionnaireID', function(req, res) {
+router.get('/:questionnaireID', function(req, res) {  // if there is a bad request ( here less parameters than required ) return status 400
 	res.status(400).json({status:"failed", reason: "Missing required parameter."});
 });
 
@@ -11,21 +11,22 @@ router.get('/:questionnaireID/:session', function(req, res) {
 	
     const { questionnaireID, session } = req.params;
 
-	pool.getConnection(function(err, connection) {
+	pool.getConnection(function(err, connection) {   //get a connection to the database from the pool
 //fasfwaf
 		if(err) {
-			res.status(500).json({status:"failed", reason: "connection to database not established."});
+			res.status(500).json({status:"failed", reason: "connection to database not established."});  //if there is an error return status 500 (Internal Server Error)
 			console.log(err);
 		}
 		else{
+			//query to select a specific session
             q = `select Answer.answer_id, Option.Question_question_id from Answer inner join mydb.Option ON Answer.Option_option_id = mydb.Option.option_id where (Option.Question_Questionnaire_questionnaire_id= ${questionnaireID} AND Answer.Session_session_id = "${session}")`;		
             connection.query(q, function(err, result) {
 
-        	if(err) {
+        	if(err) {   // if there is a bad request return status 400
 				res.status(400).json({status:"failed", reason: "Error getting question information."});
                 console.log(err);
 			}
-			else if(result==0) {
+			else if(result==0) {  // if the specific sessions doesn't exist return status 204
 				res.status(204).json({status:"failed", reason: "This session does not exist."});
                 console.log("getsessionanswers query no data");
 			}
@@ -37,7 +38,7 @@ router.get('/:questionnaireID/:session', function(req, res) {
     			}
 				console.log(answers);
 
-				if(req.query.format === "csv") {
+				if(req.query.format === "csv") { // if we want the output to be in csv
 					const csv_input = [];
 					for (const row of result) {
 						const inputty = {
@@ -55,7 +56,7 @@ router.get('/:questionnaireID/:session', function(req, res) {
                     res.status(200).send(csvData);
                     console.log("Question info OK.");
 				}
-				else {
+				else {  // else the output is in json
 					const input = {
 						"questionnaireID":questionnaireID.toString(),
 						"session":session.toString(),
@@ -67,14 +68,14 @@ router.get('/:questionnaireID/:session', function(req, res) {
                   ));
 					
                     // JSON response: default if no query format specified.					
-					res.status(200).json(response);
+					res.status(200).json(response);   // if everything worked correctly return status 200 OK
 					console.log("Question info OK.");
 				}
 
         	}	
    		});
 	}
-	connection.release();
+	connection.release();          //release the connection so someone else can use it
 	});
 
 });
