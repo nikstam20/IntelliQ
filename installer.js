26e6434bf4c29d2a49a7cd8a9d07f94055ef3aa9
ntua-el19065 <115083302+ntua-el19065@users.noreply.github.com>
2023-02-13 17:57:42 +0200

    Automated installer for npm packages in every directory

---
+++ installer.js
@@ -0,0 +1,39 @@
+const { exec } = require("child_process");
+
+
+exec("npm install --prefix ./api-backend package.json", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    exec("npm install --global --prefix ./cli pkg.json", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        exec("npm install --prefix ./frontend/frontend1 package.json", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    });
+});
+
+
+
